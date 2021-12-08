13e6f3b850904a28247b07ac06b11d97dd134ea3
Cole Rau <colevrau@gmail.com>
2021-12-08 12:40:39 -0800

    added footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Jumbotron from "./components/Jumbotron.js"
 import Navbar from "./components/Navbar.js"
 import InfoBoxTop from "./components/InfoBoxTop.js"
+import Footer from "./components/Footer.js"
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Jumbotron />
       <Navbar />
       <InfoBoxTop />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React, {useState} from "react"
+
+const Footer = () => {
+  return(
+    <div className="footer">
+      Email: xxxxxxxxx@xxxxx.com
+    </div>
+  )
+}
+
+export default Footer
