d59b63ac522ecba393285a57e099fb6d805c35b2
Cole Rau <colevrau@gmail.com>
2021-12-07 19:13:06 -0800

    created and styled navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Jumbotron from "./components/Jumbotron.js"
+import Navbar from "./components/Navbar.js"
 
 function App() {
   return (
     <div className="App">
       <Jumbotron />
+      <Navbar />
     </div>
   );
 }
