8bc6f2a5beb38cd194be695171a43de6e3cbf169
Cole Rau <colevrau@gmail.com>
2021-12-09 12:12:52 -0800

    added links from home page to all products and vice versa

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,22 +3,20 @@ import { Link } from "react-router-dom";
 
 const Navbar = props => {
   return(
-      <div>
-        <div className="navbar">
-          <span>
-            Home
-          </span>
-          <span>
-            <Link to="/all-products" style={{color: "black"}}>All Products</Link>
-          </span>
-          <span>
-            Contact
-          </span>
-          <span>
-            Instagram
-          </span>
-        </div>
+    <div>
+      <div className="navbar">
+        <Link to="/" style={{color: "black"}}><span>Home</span></Link>
+        <span>
+          <Link to="/all-products" style={{color: "black"}}>All Products</Link>
+        </span>
+        <span>
+          Contact
+        </span>
+        <span>
+          Instagram
+        </span>
       </div>
+    </div>
   )
 }
 
--- src/containers/AllProducts.js
+++ src/containers/AllProducts.js
@@ -1,11 +1,22 @@
 import React, {useState} from "react"
 import ProductCard from "../components/ProductCard.js"
 import lavender from "../photos/bodyButter/lavender.JPG"
+import Jumbotron from "../components/Jumbotron.js"
+import Navbar from "../components/Navbar.js"
+import Footer from "../components/Footer.js"
 
 const AllProducts = () => {
   return(
-    <div className="allProductsContainer">
-      <ProductCard image={lavender} type={"Body Butter"}/>
+    <div>
+      <Jumbotron />
+      <Navbar />
+      <div className="allProductsHeader">
+        All Products
+      </div>
+      <div className="allProductsContainer">
+        <ProductCard image={lavender} type={"Body Butter"}/>
+      </div>
+      <Footer />
     </div>
   )
 }
