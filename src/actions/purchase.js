4e63be10b7d5f1264667f0d8077cdb5ed32c06fd
Cole Rau <colevrau@gmail.com>
2021-12-14 12:09:21 -0800

    frontend can reach backend

--- src/actions/purchase.js
+++ src/actions/purchase.js
@@ -2,4 +2,15 @@ import axios from 'axios'
 
 export const purchase = (category, type) => {
   // axios call goes here
+  // category = broad item group (ie soaps, candles, tealights)
+  // type = scent (ie lavender, orange, rose)
+
+  axios
+  .post('http://localhost:3000/checkout', {
+    title: "Hello World!",
+    body: "test"
+  })
+  .then((response) => {
+    console.log(response)
+  })
 }
