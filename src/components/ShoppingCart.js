5f40c64eff66e9c7a3b6aa68f278c266488626e7
Cole Rau <colevrau@gmail.com>
2021-12-17 12:49:36 -0800

    added primative display of shopping cart items

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,9 +1,38 @@
 import React from "react"
+import axios from "axios"
 
-const ShoppingCart = () => {
-  return(
-    <h1>Inside Shopping Cart</h1>
-  )
+class ShoppingCart extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {products: []}
+  }
+
+  componentDidMount = () => {
+    axios
+    .get('http://localhost:3000/show-cart', {
+    })
+    .then((response) => {
+      this.setState({
+        products: [...response.data.cart]
+      })
+    })
+  }
+
+  render = () => {
+    return(
+      <div>
+        {this.state.products.length > 0 ? 
+        <div>
+          {this.state.products.map(product => <ul><li>{product}</li></ul>)}
+        </div>
+        :
+        <div>
+          Your cart is empty. 
+        </div>
+        }
+      </div>
+    )
+  }
 }
 
 export default ShoppingCart
