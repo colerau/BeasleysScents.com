4cec95351e0e2dc282ac33026acb5e78c54f2711
Cole Rau <colevrau@gmail.com>
2021-12-29 01:13:38 -0800

    added required html attribute to inputs and the select tag in checkout page

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -42,7 +42,9 @@ class Checkout extends React.PureComponent {
 
   handleSubmit = (event) => {
     event.preventDefault()
-    console.log('in handleSubmit function')
+    console.log(this.state)
+
+
     // let data = {
 
     // }
@@ -93,7 +95,7 @@ class Checkout extends React.PureComponent {
                 <div className="quicksand">
                   Address Line 1
                   <br />
-                  <input name="addressLine1" value={this.state.addressLine1} onChange={this.handleChange} />
+                  <input name="addressLine1" value={this.state.addressLine1} onChange={this.handleChange} required="required"/>
                 </div>
 
                 <br />
@@ -101,7 +103,7 @@ class Checkout extends React.PureComponent {
                 <div>
                   Address Line 2
                   <br />
-                  <input name="addressLine2" value={this.state.addressLine2} onChange={this.handleChange} />
+                  <input name="addressLine2" value={this.state.addressLine2} onChange={this.handleChange} required="required"/>
                 </div>
               
 
@@ -110,7 +112,7 @@ class Checkout extends React.PureComponent {
                 <div className="quicksand">
                   City
                   <br />
-                  <input name="city" value={this.state.city} onChange={this.handleChange} />
+                  <input name="city" value={this.state.city} onChange={this.handleChange} required="required"/>
                 </div>
             
                 <br />
@@ -118,7 +120,7 @@ class Checkout extends React.PureComponent {
                 <div className="quicksand">
                   State
                   <br />
-                  <select id="state" name="state" value={this.state.state} onChange={this.handleChange}>
+                  <select id="state" name="state" value={this.state.state} onChange={this.handleChange} required="required">
                     <option value="">---</option><option value="Alabama">Alabama</option><option value="Alaska">Alaska</option><option value="Arizona">Arizona</option><option value="Arkansas">Arkansas</option><option value="California">California</option><option value="Colorado">Colorado</option><option value="Connecticut">Connecticut</option><option value="Delaware">Delaware</option><option value="District of Columbia">District of Columbia</option><option value="Florida">Florida</option><option value="Georgia">Georgia</option><option value="Guam">Guam</option><option value="Hawaii">Hawaii</option><option value="Idaho">Idaho</option><option value="Illinois">Illinois</option><option value="Indiana">Indiana</option><option value="Iowa">Iowa</option><option value="Kansas">Kansas</option><option value="Kentucky">Kentucky</option><option value="Louisiana">Louisiana</option><option value="Maine">Maine</option><option value="Maryland">Maryland</option><option value="Massachusetts">Massachusetts</option><option value="Michigan">Michigan</option><option value="Minnesota">Minnesota</option><option value="Mississippi">Mississippi</option><option value="Missouri">Missouri</option><option value="Montana">Montana</option><option value="Nebraska">Nebraska</option><option value="Nevada">Nevada</option><option value="New Hampshire">New Hampshire</option><option value="New Jersey">New Jersey</option><option value="New Mexico">New Mexico</option><option value="New York">New York</option><option value="North Carolina">North Carolina</option><option value="North Dakota">North Dakota</option><option value="Northern Marianas Islands">Northern Marianas Islands</option><option value="Ohio">Ohio</option><option value="Oklahoma">Oklahoma</option><option value="Oregon">Oregon</option><option value="Pennsylvania">Pennsylvania</option><option value="Puerto Rico">Puerto Rico</option><option value="Rhode Island">Rhode Island</option><option value="South Carolina">South Carolina</option><option value="South Dakota">South Dakota</option><option value="Tennessee">Tennessee</option><option value="Texas">Texas</option><option value="Utah">Utah</option><option value="Vermont">Vermont</option><option value="Virginia">Virginia</option><option value="Virgin Islands">Virgin Islands</option><option value="Washington">Washington</option><option value="West Virginia">West Virginia</option><option value="Wisconsin">Wisconsin</option><option value="Wyoming">Wyoming</option>
                   </select>
                 </div>
@@ -128,7 +130,7 @@ class Checkout extends React.PureComponent {
                 <div className="quicksand">
                   Postal Code
                   <br />
-                  <input name="postalCode" value={this.state.postalCode} onChange={this.handleChange} />
+                  <input name="postalCode" value={this.state.postalCode} onChange={this.handleChange} required="required"/>
                 </div>
               
                 <br />
