bc1b525427be4f73def9945a413c623b447db45d
Cole Rau <colevrau@gmail.com>
2022-01-05 11:21:42 -0800

    added more products

--- src/containers/MoreProducts.js
+++ src/containers/MoreProducts.js
@@ -4,6 +4,7 @@ import Jumbotron from "../components/Jumbotron.js"
 import Navbar from "../components/Navbar.js"
 import Footer from "../components/Footer.js"
 import lotionBar from "../photos/moreProducts/lotionBar.jpg"
+import lipButter from "../photos/moreProducts/lipButter.jpg"
 
 const MoreProducts = props => {
 
@@ -21,8 +22,11 @@ const MoreProducts = props => {
       </div>
       
       <div className="allProductsContainer">
+        <ProductCard image={lipButter} category={"Lip Butter"} orientation="landscape" scent="Vanilla Mint" price="5.00" />
+        <ProductCard image={lipButter} category={"Lip Butter"} orientation="landscape" scent="Honey Mint" price="5.00" />
         <ProductCard image={lotionBar} category={"Solid Lotion Bar"} price="10.00" />
       </div>
+
       <Footer />
     </div>
   )
