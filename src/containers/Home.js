52b078834ecde5ccfdf8faa27cba67cb1f2ccb9f
Cole Rau <colevrau@gmail.com>
2022-01-05 12:08:50 -0800

    added quote to home page

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -12,6 +12,34 @@ const Home = () => {
       <Navbar />
       <InfoBoxTop />
       <InfoBoxBottom />
+
+      <br /> <br />
+
+      <div id="infoSectionHomePage">
+        <div id="quoteSectionHomePage">
+          <div id="textSectionHomePage" className="quicksand">
+
+            <div style={{fontSize: "24px"}}>
+              My Inspiration
+            </div>
+
+            <br />
+
+            <div style={{fontSize: "20px"}}>
+              I started this journey because of my family skin history ranging from eczema, psoriasis, and acne. I have always struggled when it came time to purchase the right products for my family and never really had any luck. So, I decided to create all-natural bath and body products to share with my family and others in my community. 
+            </div>
+      
+            <br />
+            <span style={{fontSize: "18px"}}>
+              <strong>~</strong> Misty Beasley
+            </span>
+
+          </div>
+        </div>
+      </div>
+
+      <br />
+
       <Footer />
     </div>
   )
