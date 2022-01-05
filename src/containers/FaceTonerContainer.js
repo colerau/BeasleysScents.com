f9045d3778907dd69b7fcb13d2e7c714ac3ac2ec
Cole Rau <colevrau@gmail.com>
2022-01-05 07:12:50 -0800

    added face toners

---
+++ src/containers/FaceTonerContainer.js
@@ -0,0 +1,56 @@
+import React from "react"
+import Jumbotron from "../components/Jumbotron.js"
+import Navbar from "../components/Navbar.js"
+import Footer from "../components/Footer.js"
+
+const BathBombsContainer = props => {
+
+  return(
+    <div>
+      <Jumbotron />
+      <Navbar />
+      <div className="header">
+        Choose your Bath Bomb
+      </div>
+
+      <div className="quicksand" style={{textAlign: "center", fontSize: "19px", paddingBottom: "30px"}}>
+        Email Misty Beasley at mistybeasley@beasleyscents.com to order. 
+      </div>
+
+      <div className="quicksand" style={{fontSize: "34px", textAlign: "center", paddingBottom: "20px"}}>
+        Scents
+      </div>
+
+      <div className="quicksand" style={{fontSize: "20px", textAlign: "center"}}>
+        <span>Lavender &amp; Rose</span>
+      </div>
+
+      <br />
+
+      <div className="quicksand" style={{fontSize: "20px", textAlign: "center"}}>
+        <span>Grapefruit &amp; Sweet Orange</span>
+      </div>
+
+      <br />
+
+      <div className="quicksand" style={{fontSize: "20px", textAlign: "center"}}>
+        <span>Grapefruit &amp; Lemon</span>
+      </div>
+
+      <br />
+
+      <div className="quicksand" style={{fontSize: "20px", textAlign: "center"}}>
+        <span>Sandalwood, Helichrysum &amp; Lavender</span>
+      </div>
+
+      <Footer />
+    </div>
+  )
+}
+
+export default BathBombsContainer
+
+
+
+
+
